50f5412abdf16f41c733fac7f45e11c3ba4f14a5
Jesse van den Kieboom <jesse@icecrew.nl>
2010-10-21 23:34:29 +0200

    Added config stubs

--- data/webots.conf.ex
+++ data/webots.conf.ex
@@ -3,3 +3,8 @@
 ### Whether webots is checked for the correct permissions etc.
 # secure = no
 
+### The webots version
+# version = 6.1.5
+
+### Whether or not to force webots in batch mode
+# force-batch = no
